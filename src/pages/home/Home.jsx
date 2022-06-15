54e8290c4da2c4ed9013149f42ee29389ffb4c59
mitibirru <357miteshbirru@gmail.com>
2022-06-15 17:39:03 +0530

    added property list and updated home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,7 @@
+import Featured from "../../components/featured/Featured";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
+import PropertyList from "../../components/propertyList/PropertyList";
 import "./home.css";
 
 const Home = () => {
@@ -7,7 +9,12 @@ const Home = () => {
     <div>
       <Navbar />
       <Header />
-      
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="homeTitle">Home guests love</h1>
+      </div>
     </div>
   );
 };
